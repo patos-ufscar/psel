4c815cb6e3109724f34df85800686bf5ac95bde2
Gabriel <82649581+Psor1107@users.noreply.github.com>
2024-09-01 12:40:07 -0300

    Setting up Maven environment

---
+++ reverse-proxy-file-server/src/main/java/com/ufscar/psor/Main.java
@@ -0,0 +1,7 @@
+package com.ufscar.psor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nQuack, Quack!\n  __\n<(o )___\n (  ._> /\n  `---' ");
+    }
+}
