3fa9d249f7aad78e87be08e908886c7da77b6042
Gabriel <82649581+Psor1107@users.noreply.github.com>
2024-09-01 20:04:18 -0300

    Basic Sockets implementation

--- reverse-proxy-file-server/src/main/java/com/ufscar/psor/Main.java
+++ reverse-proxy-file-server/src/main/java/com/ufscar/psor/Main.java
@@ -1,7 +1,27 @@
 package com.ufscar.psor;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
+
+    private static final int PORT = 1107;
+
     public static void main(String[] args) {
-        System.out.println("\nQuack, Quack!\n  __\n<(o )___\n (  ._> /\n  `---' ");
+        try {
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                System.out.println("escutando na porta: " + PORT);
+                while (true) {
+                    try (Socket clientSocket = serverSocket.accept(); OutputStream outputStream = clientSocket.getOutputStream()) {
+                        outputStream.write("\nQuack, Quack!\n  __\n<(o )___\n (  ._> /\n  `---' ".getBytes());
+                        outputStream.flush();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
     }
 }
