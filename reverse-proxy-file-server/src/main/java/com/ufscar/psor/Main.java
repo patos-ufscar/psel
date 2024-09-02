b50859e115dbeb810f6e6fcce3e90d93679e7dc2
Gabriel <82649581+Psor1107@users.noreply.github.com>
2024-09-02 11:45:52 -0300

    HTTP implementation

--- reverse-proxy-file-server/src/main/java/com/ufscar/psor/Main.java
+++ reverse-proxy-file-server/src/main/java/com/ufscar/psor/Main.java
@@ -1,6 +1,7 @@
 package com.ufscar.psor;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,13 +14,30 @@ public class Main {
         try {
             try (ServerSocket serverSocket = new ServerSocket(PORT)) {
                 System.out.println("escutando na porta: " + PORT);
+                
                 while (true) {
-                    try (Socket clientSocket = serverSocket.accept(); OutputStream outputStream = clientSocket.getOutputStream()) {
-                        outputStream.write("\nQuack, Quack!\n  __\n<(o )___\n (  ._> /\n  `---' ".getBytes());
-                        outputStream.flush();
+                    try (Socket clientSocket = serverSocket.accept();) {
+                        System.out.println("cliente conectado: " + clientSocket.getInetAddress().getHostAddress());
+                        handleClient(clientSocket);
                     }
+                        
                 }
+
             }
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static void handleClient(Socket clientSocket) throws IOException {
+        try (OutputStream outputStream = clientSocket.getOutputStream(); 
+            InputStream inputStream = clientSocket.getInputStream()) 
+        {
+            //outputStream.write("\nQuack, Quack, converse com o pato!\n  __\n<(o )___\n (  ._> /\n  `---' \n".getBytes());
+            //byte[] buffer = new byte[2048];
+            //inputStream.read(buffer);
+
+            outputStream.write("HTTP/1.1 200 OK\r\n\r\n<h1>Quack, Quack! Voce achou o pato!<h1> <pre>  __\n<(o )___\n (  ._> /\n  `---' </pre>".getBytes()); 
         } catch (IOException e) {
             e.printStackTrace(System.out);
         }
