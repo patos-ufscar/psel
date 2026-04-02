46c785374865909162e61e400a39006f3eeec0fb
gdefaria <gdfaria@proton.me>
2026-04-02 09:15:13 -0300

    refactor: remove server and balancer packages. Basically restarting from scratch

--- main.go
+++ main.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-	"loadbal/server"
-	"loadbal/balancer"
 )
 
 func main() {
-	servers := []*server.Server{
-		server.New("localhost:8001"),
-	}
 
-	balancer, err := balancer.New("localhost:8000", servers)
-
-	if err != nil {
-		panic(err)
-	}
-
-	balancer.Start()
-	fmt.Println("Started")
 }
