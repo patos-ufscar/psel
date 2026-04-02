9eb2cf0bc9b7489083326920ff4bcd635631ae0d
gdefaria <gdfaria@proton.me>
2026-04-02 19:01:36 -0300

    feat: random balancing strategy

--- balancer/strategies.go
+++ balancer/strategies.go
@@ -1,13 +1,23 @@
 package balancer
 
+import (
+	"math/rand"
+)
+
 type Strategy func(balancer *Balancer) string
 
-func static (b *Balancer) string {
+func static(b *Balancer) string {
 	return b.Servers[0]
+}
+
+func random(b *Balancer) string {
+	return b.Servers[rand.Intn(len(b.Servers))]
 }
 
 var Strategies = struct {
 	Static Strategy
+	Random Strategy
 } {
 	Static: static,
+	Random: random,
 }
