f570f1257d03ca908904f2cd78523289e047a889
Italo Eduardo <itzdasensi@gmail.com>
2026-05-21 05:53:28 +0000

    estrutura inicial do projeto

---
+++ rules.h
@@ -0,0 +1,24 @@
+#ifndef RULES_H
+#define RULES_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+typedef enum {
+    ALLOW,
+    DENY,
+    TARPIT
+} TypeAct;
+
+
+typedef struct
+{
+    bool verbose;
+    TypeAct act;
+    uint32_t ip;
+} Rule;
+
+Rule* load_rules(const char *filename, int *total_rules);
+
+#endif
